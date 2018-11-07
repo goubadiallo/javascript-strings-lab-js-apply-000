ea4bc3e10491797281c3ae0612858a15cef58b8b
Mamadou Diallo <mamadou.diallo@wework.com>
2018-11-07 18:50:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
+<<<<<<< HEAD
 var greetSpecialGuest = "Hello, " + specialGuest + "!"
+=======
+var greetSpecialGuest = "hello" + specialGuest + "!";
+>>>>>>> fd189b32d9b980fdff4e38d8737f4ea06e26713d
 
 var topic = "space";
 var conversation = "Let\'s talk about"
